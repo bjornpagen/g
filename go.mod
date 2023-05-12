00f3a42ee16c6af451a94fa3fab4544d03539924
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2023-05-12 18:13:38 +0200

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bjornpagen/g
+
+go 1.20
---
+++ pkg/gapbuf/gapbuf.go
@@ -0,0 +1,110 @@
+package gapbuf
+
+const (
+	bufferMin = 64*1024 - 1
+)
+
+type GapBuffer struct {
+	gapStart int
+	gapEnd   int
+	buf      []rune
+}
+
+func (b *GapBuffer) gapSize() int {
+	return b.gapEnd - b.gapStart
+}
+
+func (b *GapBuffer) moveGap(position int) {
+	if position == b.gapStart {
+		return
+	} else if position < b.gapStart {
+		d := b.gapStart - position
+		b.gapStart -= d
+		b.gapEnd -= d
+		copy(b.buf[b.gapEnd:], b.buf[b.gapStart:b.gapStart+d])
+	} else {
+		d := position - b.gapStart
+		b.gapStart += d
+		b.gapEnd += d
+		copy(b.buf[b.gapStart-d:b.gapStart], b.buf[b.gapEnd-d:b.gapEnd])
+	}
+}
+
+func (b *GapBuffer) ensureGap(length int) {
+	if b.gapSize() >= length {
+		return
+	}
+
+	newSize := len(b.buf) + length + bufferMin
+	endSize := len(b.buf) - b.gapEnd
+
+	buf := make([]rune, newSize)
+	copy(buf, b.buf[:b.gapStart])
+	copy(buf[newSize-endSize:], b.buf[b.gapEnd:])
+
+	b.buf = buf
+	b.gapEnd = newSize - endSize
+}
+
+func (b *GapBuffer) deleteBuffer(position, length int) {
+	b.moveGap(position)
+	b.gapEnd += length
+}
+
+func (b *GapBuffer) readBuffer(c []rune, position int) (n int) {
+	if position < b.gapStart {
+		d := b.gapStart - position
+		n += copy(c[:d], b.buf[position:b.gapStart])
+		n += copy(c[d:], b.buf[b.gapEnd:])
+
+		return n
+	}
+
+	position += b.gapSize()
+	return copy(c, b.buf[position:])
+}
+
+func (b *GapBuffer) insertBuffer(position int, s []rune) {
+	b.ensureGap(len(s))
+	b.moveGap(position)
+	copy(b.buf[b.gapStart:], s)
+	b.gapStart += len(s)
+}
+
+type Buffer struct {
+	numRunes int
+	gb       *GapBuffer
+}
+
+func New() Buffer {
+	return Buffer{
+		gb: &GapBuffer{
+			buf:    make([]rune, bufferMin),
+			gapEnd: bufferMin,
+		},
+	}
+}
+
+func (b *Buffer) Read(p []rune, length, position int) (n int) {
+	if position+length > b.numRunes {
+		length = b.numRunes - position
+	}
+	if length <= 0 {
+		return 0
+	}
+
+	return b.gb.readBuffer(p, position)
+}
+
+func (b *Buffer) Insert(runes []rune, position int) {
+	if len(runes) > 0 {
+		b.gb.insertBuffer(position, runes)
+		b.numRunes += len(runes)
+	}
+}
+
+func (b *Buffer) Delete(startPosition, endPosition int) {
+	length := endPosition - startPosition
+	b.gb.deleteBuffer(startPosition, length)
+	b.numRunes -= length
+}
